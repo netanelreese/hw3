e02b8250f33aef9ba318abb412b94c79bc10e594
Reese <Nathanael.G-CTR.Reese@faa.gov>
2022-10-14 15:50:52 -0500

    Added hw java files :

---
+++ HW3_Problem2_Group-30.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.sql.*;
+
+public class problem2 {
+	Scanner input - new Scanner(System.in);
+	public static void main(String[] args) {
+		String query = "";
+		
+		System.out.println("Enter Which Option you would like\n1.\n2.\n3.\n4. Exit\n");
+		userInp = input.next();
+		while(String userInp != null) {
+		
+		if(userInp.contains("1")) {
+			q
+		}
+		else if(userInp.contains("2")) {
+		}
+		else if(userInp.contains("3")) {
+		}
+		else if(userInp.contains("4")){
+			System.out.println("Exiting...");
+			System.exit();
+		}
+		else{
+			System.out.println("Invalid Input");
+		}
+		System.out.println("Enter Which Option you would like\n1.\n2.\n3.\n4. Exit\n");
+		userInp = input.next();
+		}
+
+	}
+
+}
